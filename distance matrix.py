659e6b10c7e99cee1378e3a717df67a6d8f65119
Sanjeevan Vivekanantha <63479459+Sanjeevan-informatik@users.noreply.github.com>
2023-02-06 14:40:07 +0100

    Add files via upload

---
+++ distance matrix.py
@@ -0,0 +1,140 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[143]:
+
+
+from numpy import where
+import numpy as np
+import pandas as pd
+from numpy import fill_diagonal
+from pandas import DataFrame
+from itertools import combinations
+import glob
+import sys
+from Bio import SeqIO
+import json
+
+
+'''
+this code will explain how to compute the distance matrix for multiple sequence aligen data.
+'''
+
+def pairwise_dist(first_seq, second_seq):
+    '''
+    compute the pairwise distances between two sequences
+    '''
+    total_distance = 0
+    
+    if len(first_seq) > len(second_seq):
+        total_distance =  len(first_seq) - len(second_seq)
+    elif len(second_seq) > len(first_seq):
+        total_distance = len(second_seq) - len(first_seq)
+    
+    
+    for char_1, char_2 in zip(first_seq, second_seq):
+        if char_1 != char_2:
+            total_distance += 1
+    return total_distance
+
+
+def distance_matrix(dist_dict):
+    '''
+   Create a blank dataframe and label its column and index headings
+   with all of the creatures.
+    '''
+    dist_matrix = DataFrame(index=dist_dict.keys(), columns=dist_dict.keys())
+    fill_diagonal(dist_matrix.values, 0)
+    return dist_matrix
+
+
+def generate_distance_matrix(dist_matrix, pairwise_dict, start, counter, end):
+    '''
+    Utilize vectorized assignments to fill in the complete distance matrix.
+    Imagine allocating lists for each organism that are at right angles and have a vertex of zero.
+    Setting the start position to the end position and adding a decrementing counter value to the
+    end value update the start and end positions for the right angles.
+    '''
+    for i, j in zip(range(len(sequence_dict.keys()) - 1), range(1, len(sequence_dict.keys()))):
+        sequence_df.iloc[i, j:] = list(pairwise_dict.values())[start:end]
+        sequence_df.iloc[j:, i] = sequence_df.iloc[i, j:]
+        start = end
+        end += counter
+        counter -= 1
+    return dist_matrix
+
+
+
+# In[144]:
+
+
+'''
+ input : this block of code will take the JSON dataset which contains the sample name and their genotype data 
+ which is present in array format 
+ 
+ output : this block would generate the distance matrix of multiple sequence 
+ 
+'''
+
+f = open('data/sample.json')
+data = json.load(f)
+df = pd.DataFrame(data["numbers_of_alternate_alleles"], index=data["samples_selected_mapped"])
+df = df.astype(str)
+columns=df.index
+sequence_dict = {}
+    
+for x in range(len(columns)):
+    
+    sequence_dict[columns[x]] = str(''.join(df.iloc[x]))
+        
+sequence_df = distance_matrix(sequence_dict)
+Seq_pairwise_diff = {
+    key: pairwise_dist(sequence_dict[key[0]], sequence_dict[key[1]])
+    for key in combinations(sequence_dict, 2)
+}
+    
+Seq_df = generate_distance_matrix(sequence_df, Seq_pairwise_diff, 0,
+                        len(sequence_dict.keys()) - 2,
+                        len(sequence_dict.keys()) - 1)
+
+
+# In[145]:
+
+
+Seq_df
+
+
+# In[146]:
+
+
+'''
+    input : this block of code will take the fattest dataset which contains the sample name and their sequence data 
+        which is present in array format
+ 
+    output : this block would generate the distance matrix of multiple sequence 
+ 
+'''
+
+all_fasta_file = "data/vcf_to_seq.fasta"
+sequence_list = [] # To keep order of sequence
+sequence_dict = {}
+for record in SeqIO.parse(open(all_fasta_file, "r"), "fasta"):
+    sequence_list.append(record.id)
+    sequence_dict[record.id] = str(record.seq)
+
+sequence_df = distance_matrix(sequence_dict)
+Seq_pairwise_diff = {
+    key: pairwise_dist(sequence_dict[key[0]], sequence_dict[key[1]])
+    for key in combinations(sequence_dict, 2)
+}
+
+Seq_df = generate_distance_matrix(sequence_df, Seq_pairwise_diff, 0,
+                            len(sequence_dict.keys()) - 2,
+                            len(sequence_dict.keys()) - 1)
+
+
+# In[147]:
+
+
+Seq_df
+
